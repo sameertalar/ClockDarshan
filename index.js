80ac275dcc411cf11cf55022f52e85a52de3c8da
Sameer Talar <sameer.talar@uhc.com>
2023-05-17 15:34:59 -0400

    code cleanup

--- index.js
+++ index.js
@@ -1,29 +1,10 @@
 $(document).ready(function () {
-  var __AppEnabledStatus = false;
-  var __ClockdarshanStatus = false;
   var __ApiCallStatus = false;
 
   const _UpdaterApiUrl =
     "https://script.google.com/macros/s/AKfycbyI_7nngMEAJIF0K-i7XAi9u1wyjHupw0uNK9uk7qec/dev";
   const _ResetApiUrl =
     "https://script.google.com/macros/s/AKfycbw8xlLx02pJJWyaJIFMNdsT_h-C04drUlpFZeCVb4v1/dev";
-
-  var _Tracks = [];
-
-  const _gods = [
-    "गणेश",
-    "महाराज",
-    "दुर्गा",
-    "शंकर",
-    "महालक्ष्मी",
-    "अन्नपूर्णा",
-    "हनुमान",
-    "रमण म.",
-    "सरस्वती",
-    "नर्मदा",
-    "बुद्ध",
-    "कृष्ण",
-  ];
 
   const _configs = [
     {
@@ -85,24 +66,10 @@ $(document).ready(function () {
     },
   ];
 
-  function getDivBole(id, txt) {
-    let div = document.createElement("div");
-    if (id) div.id = id;
-    div.className = "row ";
-    div.innerText = txt;
-
-    return div;
-  }
-
-  function createChunkDiv(data, currentRow, praharStartRow) {
+  function createChunkDiv(data, currentRow) {
     let divRow = document.createElement("div");
     if (data.row) divRow.id = "row" + data.row;
     divRow.className = "row ";
-
-    /*
-    if (data.chunk.includes("-4") && praharStartRow + 11 !== data.row)
-      divRow.className = divRow.className + "border-top rounded-top  border-2";
-*/
 
     if (currentRow === data.row)
       divRow.className = divRow.className + " bg-highlight";
@@ -112,7 +79,6 @@ $(document).ready(function () {
     // col1
     let divC1 = document.createElement("div");
     divC1.className = "col-2 px-0   text-center";
-    //divC1.innerText = data.value;
 
     let i1 = document.createElement("i");
 
@@ -178,8 +144,6 @@ $(document).ready(function () {
   }
 
   function postToGoogle(update) {
-    // $("#progress-loading").show();
-
     $("#progress-modal").modal("show");
     __ApiCallStatus = true;
 
@@ -208,10 +172,6 @@ $(document).ready(function () {
       success: function (data, textStatus, xhr) {
         console.log("Api Response Data:", data);
 
-        // $("#containerPath").html("Sameer" + data);
-
-        // document.getElementById("containerPath").appendChild(boleElement);
-
         try {
           if (data && data.chunks) {
             $("#hiddenLoggedRow").val(data.loggedRow);
@@ -226,11 +186,7 @@ $(document).ready(function () {
             $("#containerPath").html("");
 
             for (let i = data.chunks.length - 1; i >= 0; i--) {
-              var divRow = createChunkDiv(
-                data.chunks[i],
-                data.currentRow,
-                data.praharStartRow
-              );
+              var divRow = createChunkDiv(data.chunks[i], data.currentRow);
               document.getElementById("containerPath").appendChild(divRow);
 
               console.log(
@@ -253,83 +209,6 @@ $(document).ready(function () {
           $("#errorMessage").html("Unhandled Error postToGoogle: " + err);
         }
 
-        /*
-        try {
-          if (data && data.chunks) {
-            for (let i = 0; i < data.chunks.length; i++) {
-              $("#lblChunkQuarter" + data.chunks[i].id).html(
-                _Tracks.find((x) => x.row === data.chunks[i].row).quarter
-              );
-              $("#btnChunkPush" + data.chunks[i].id).attr(
-                "data-row",
-                data.chunks[i].row
-              );
-
-              // $("#lblChunkE" + data.chunks[i].id).html(data.chunks[i].e);
-              // $("#lblChunkW" + data.chunks[i].id).html(data.chunks[i].w);
-              // $("#lblChunkM" + data.chunks[i].id).html(data.chunks[i].m);
-              $("#lblChunkM" + data.chunks[i].id).removeClass();
-              $("#lblChunkW" + data.chunks[i].id).removeClass();
-              $("#lblChunkE" + data.chunks[i].id).removeClass();
-              $("#divChunk" + data.chunks[i].id).removeClass("bg-danger");
-
-              if (data.chunks[i].m === "X" || data.chunks[i].m === "")
-                $("#divChunk" + data.chunks[i].id).addClass("bg-danger");
-
-              $("#lblChunkM" + data.chunks[i].id).addClass(
-                getFaClass(data.chunks[i].m)
-              );
-              $("#lblChunkW" + data.chunks[i].id).addClass(
-                getFaClass(data.chunks[i].w)
-              );
-              $("#lblChunkE" + data.chunks[i].id).addClass(
-                getFaClass(data.chunks[i].e)
-              );
-
-              $("#lblChunkGod" + data.chunks[i].id).html(
-                _Tracks.find((x) => x.row === data.chunks[i].row).god
-              );
-            }
-            // showAlert("Deep Breath");
-            //showAlert("<p>" + data + "</p>");
-            // $("#errorMessage").html(data.lq);
-            $("#hiddenLoggedRow").val(data.lr);
-            $("#hiddenLastCallRow").val(data.chunks[0].row);
-
-            $("#lblM5").html(data.count.mind.m5);
-            $("#lblM4").html(data.count.mind.m4);
-            $("#lblM3").html(data.count.mind.m3);
-            $("#lblM2").html(data.count.mind.m2);
-            $("#lblM1").html(data.count.mind.m1);
-
-            $("#lblE3").html(data.count.energy.e3);
-            $("#lblE2").html(data.count.energy.e2);
-            $("#lblE1").html(data.count.energy.e1);
-
-            $("#lblW5").html(data.count.work.w5);
-            $("#lblW4").html(data.count.work.w4);
-            $("#lblW3").html(data.count.work.w3);
-            $("#lblW2").html(data.count.work.w2);
-            $("#lblW1").html(data.count.work.w1);
-
-            if (update && $(".checkEdit:checked").val() == "T") {
-              if (data.lr === getCurrentTrackerTimeRow()) {
-                $("#checkEdit").prop("checked", false);
-                $("#radioMindA").click();
-              }
-            }
-          } else {
-            $("#errorMessage").html(" Data Not Returned by Api. ");
-          }
-        } catch (err) {
-          console.log("Unhandled Error postToGoogle", err);
-          $("#containerPath").html(err);
-          $("#errorMessage").html("Unhandled Error postToGoogle: " + err);
-        }
-*/
-        // reloadStatusSheet();
-
-        // $("#progress-loading").hide();
         $("#progress-modal").modal("hide");
         __ApiCallStatus = false;
       },
@@ -337,50 +216,13 @@ $(document).ready(function () {
         $("#errorMessage").html(error_text);
         console.log("error_text", error_text);
         $("#containerPath").html(error_text);
-        //$("#progress-loading").hide();
         $("#progress-modal").modal("hide");
       },
     });
   }
 
-  populateTracker();
-  populateSelectTrackerTime();
-
-  function populateTracker() {
-    g = 0;
-
-    for (let h = 0; h < 24; ++h) {
-      for (let q = 1; q <= 4; ++q) {
-        _Tracks.push({
-          row: getTrackerRow(h, q),
-          quarter: (h % 12 || 12).toString().padStart(2, "0") + "-" + q + "",
-          god: _gods[g],
-        });
-
-        if (g === 11) g = 0;
-        else g++;
-      }
-    }
-
-    // console.log(_Tracks);
-  }
-
   function getTrackerRow(h, q) {
     return h * 4 + q + 3;
-  }
-
-  function populateSelectTrackerTime() {
-    for (let i = 0; i < _Tracks.length; i++) {
-      $("#selectTrackerTime").append(
-        $("<option class='text-primary bg-white'></option>")
-          .attr("value", _Tracks[i].row)
-          .text(_Tracks[i].quarter)
-          .attr("data-row", _Tracks[i].row)
-          .attr("data-god", _Tracks[i].god)
-      );
-    }
-
-    setTrackerTime();
   }
 
   function eyeOpenClose() {
@@ -393,10 +235,6 @@ $(document).ready(function () {
 
   eyeOpenClose();
   setInterval(eyeOpenClose, 2000);
-
-  function setTrackerTime() {
-    $("#selectTrackerTime").val(getCurrentTrackerTimeRow());
-  }
 
   function showCurrentSelection() {
     $("#centerCoreD").html(
@@ -435,30 +273,6 @@ $(document).ready(function () {
         (100 - elaspePercentQ) +
         "% )"
     );
-
-    $("#bodyCD").css("background-size", elaspePercentQ + "%");
-
-    /*
-    if (Number($("#centerCoreD").html()) > 1) {
-      $("#bodyCD").css(
-        "background-image",
-        "linear-gradient(#ff2e2d,white,white,white,#ff2e2d ,#ff2e2d )"
-      );
-      $("#bodyCD").css("background-size", "100%");
-    } else if (Number($("#centerCoreD").html()) === 1) {
-      $("#bodyCD").css(
-        "background-image",
-        "linear-gradient(#f4b400,white,white,white,white ,white,white,#f4b400 )"
-      );
-      $("#bodyCD").css("background-size", elaspePercentQ + "%");
-    } else {
-      $("#bodyCD").css(
-        "background-image",
-        "linear-gradient(#babec4,white,white,white,white ,white,white,#babec4 )"
-      );
-      $("#bodyCD").css("background-size", elaspePercentQ + "%");
-    }
-*/
   }
 
   function showCurrentTimeLeft(hours, min, sec) {
@@ -533,25 +347,6 @@ $(document).ready(function () {
     }
   }
 
-  function Mind_Change() {
-    $("#selectedMp").removeClass();
-
-    let selfa = getFaClass($(".radioMind:checked").val());
-    $("#selectedMp").addClass(selfa);
-  }
-  function Work_Change() {
-    $("#selectedWp").removeClass();
-    let selfa = getFaClass($(".radioWork:checked").val());
-
-    $("#selectedWp").addClass(selfa);
-  }
-  function Energy_Change() {
-    $("#selectedEp").removeClass();
-    let selfa = getFaClass($(".radioEnergy:checked").val());
-
-    $("#selectedEp").addClass(selfa);
-  }
-
   page_Load();
 
   __AppEnabledStatus = setInterval(processBeHereNow, 1000);
@@ -571,21 +366,7 @@ $(document).ready(function () {
     let deg = getOnePointerDegree(hourDeg, minDeg);
     r(onepointerBase, deg);
 
-    if ($(".checkEdit:checked").val() != "T") {
-      r(onepointerCircle, deg);
-      //console.log(hourDeg,minDeg);
-    } else {
-      let selectedPeriod = parseInt(
-        $("#selectTrackerTime option:selected").val()
-      );
-      let hour2 = Math.floor((selectedPeriod - 4) / 4) % 12 || 12;
-      let quarter2 = (selectedPeriod - 4) % 4;
-      let hourDeg2 = 30 * (hour2 % 12);
-      let minDeg2 = quarter2 * 90;
-
-      let deg2 = getOnePointerDegree(hourDeg2, minDeg2);
-      r(onepointerCircle, deg2);
-    }
+    r(onepointerCircle, deg);
 
     //
   }, 1000);
@@ -603,7 +384,6 @@ $(document).ready(function () {
   }
 
   function page_Load() {
-    $("#selectTrackerTime").focus();
     $("#success-alert").hide();
     $("#radio-mind-3").click();
 
@@ -614,19 +394,6 @@ $(document).ready(function () {
     $("#clock-container").on("click", oneClickTracker);
     $("#ClockInnerCircle").on("click", oneClickTracker);
     $("#centerCoreD").on("click", oneClickTracker);
-
-    $("#btnChunkPush4").on("click", function (event) {
-      setTracketSelect($("#" + this.id).data("row"));
-    });
-    $("#btnChunkPush3").on("click", function (event) {
-      setTracketSelect($("#" + this.id).data("row"));
-    });
-    $("#btnChunkPush2").on("click", function (event) {
-      setTracketSelect($("#" + this.id).data("row"));
-    });
-    $("#btnChunkPush1").on("click", function (event) {
-      setTracketSelect($("#" + this.id).data("row"));
-    });
 
     postToGoogle(false);
   }
@@ -651,17 +418,8 @@ $(document).ready(function () {
 
       $("#hiddenCurentRow").val(getCurrentTrackerTimeRow());
 
-      if ($(".checkEdit:checked").val() != "T") {
-        setTrackerTime();
-
-        if (
-          Number($("#hiddenLoggedRow").val()) <
-          Number($("#selectTrackerTime option:selected").val())
-        ) {
-          $("body").addClass("bag");
-        } else {
-          $("body").removeClass("bag");
-        }
+      if (Number($("#hiddenLoggedRow").val()) < getCurrentTrackerTimeRow()) {
+        $("body").addClass("bag");
       } else {
         $("body").removeClass("bag");
       }
@@ -677,7 +435,7 @@ $(document).ready(function () {
         Number($("#hiddenCurentRow").val()) !==
           Number($("#hiddenLastCallRow").val())
       ) {
-        //  postToGoogle(false);
+        postToGoogle(false);
         // console.log("1 mins data Lag Api Called");
       }
     } catch (error) {
@@ -685,24 +443,6 @@ $(document).ready(function () {
       $("#errorMessage").html("Unhandled Error: " + error);
     }
   }
-
-  //-----------------------Old Code---------------------------------------
-
-  function setTracketSelect(id) {
-    //console.log("Selected", $("#hiddenTime"+id).val());
-
-    $("#selectTrackerTime").val(id);
-
-    if (Number($("#hiddenCurentRow").val()) === id) {
-      $("#radio-mind-3").click();
-      $("#checkEdit").prop("checked", false);
-    } else {
-      $("#radio-mind-3").click();
-      $("#checkEdit").prop("checked", true);
-    }
-  }
-
-  //
 
   function oneClickResetSheet() {
     if (confirm("Are you sure want to reset?")) {
